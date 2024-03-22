3708d40d966d51ef1016c2a559ae2c73bb58d328
Daniel Carmona Zapata <dcz9412@gmail.com>
2024-03-22 13:09:29 -0500

    BACK: server has been created

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,12 @@
 import express from 'express';
+
+const app = express();
+
+//
+app.get( '/user', (req, res) => {
+
+    res.send( 'users' )
+} );
+
+
+app.listen( 3006, () => { console.log( 'app run on port 3006' ) } )
