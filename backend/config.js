088115d37353e145d521e32c08ddd226c4befaab
Daniel Carmona Zapata <dcz9412@gmail.com>
2024-03-25 15:08:28 -0500

    config.js

--- backend/config.js
+++ backend/config.js
@@ -1,5 +1,5 @@
 //https://testing-express-app-kbum.vercel.app/
-export const FRONTED_URL =  process.env.FRONTED_URL || "https://testing-express-app-kbum.vercel.app/";
+export const FRONTED_URL =  process.env.FRONTED_URL || "testing-express-app-kbum-git-main-dcz9412-gmailcom.vercel.app";
 
 export const PORT = process.env.PORT || 3007;
 
