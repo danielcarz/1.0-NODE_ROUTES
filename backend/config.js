b17c3f45c61c6b99f46e33a893b6a2fe75d0d6f2
Daniel Carmona Zapata <dcz9412@gmail.com>
2024-04-03 14:22:23 -0500

    conection to data base:

--- backend/config.js
+++ backend/config.js
@@ -1,8 +1,16 @@
-const FRONTED_URL = process.env.FRONTED_URL || "http://localhost:5173"; //ruta vite
+const express = require('express');
 
+//fronted url
+const FRONTED_URL = process.env.FRONTED_URL  || "http://localhost:5173"; //ruta vite
+
+// port app
 const PORT = process.env.PORT || 3008;
 
-module.exports = {
+//contection to database
+DB_CONECTION = "mongodb+srv://db_user_one:E9ewP57AHDmz1oyd@cluster0.hma87kd.mongodb.net/testing_routes";
+
+module.exports = { 
     FRONTED_URL,
-    PORT
+    PORT,
+
 };
--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
+
 
 const { FRONTED_URL, PORT } = require('./config.js');
 
-const HandleRoutes = require('./Routes/HandleRoutes.js');
+const HandleRoutes = require('./Routes/HandleRoutes.js'); //routes
+const { db_conection } = require( './Database/config.js' ); // database
 
 const app = express();
 
+//db conection
+db_conection();
+
 //cors
-app.use(cors({ origin: FRONTED_URL }));
+app.use(cors({ origin: FRONTED_URL })); 
 
 //parse
 app.use(express.json());
